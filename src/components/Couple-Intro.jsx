bcf93ec92663b6fda6999cc5b300993746fadee8
missael306 <40796103+missael306@users.noreply.github.com>
2024-10-28 23:35:23 -0600

    papas y padrinos

--- src/components/Couple-Intro.jsx
+++ src/components/Couple-Intro.jsx
@@ -17,41 +17,27 @@ const Intro = () => {
                 <div className="shape_2"><Image src={require('../assets/images/intro/bg_shape_2.png')} className="img-fluid" alt="side-img" width="100%" /></div>
                 <Container>
                     <div className="row align-items-center z_index">
-                        <div className="col-lg-3 text-center text-lg-end bride wow fadeInDown" data-wow-delay="0.1s" data-wow-duration="1.2s" >
-                            <div className="couple-img">
-                                <Image src={require('../assets/images/intro/2.jpg')} alt="image" className='img-fluid' width="100%" />
-                            </div>
-                            <h3>Raviraj Jethva</h3>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                            <div className="social">
-                                <ul>
-                                    <li><a href="#/"><i className="ti-facebook"></i></a></li>
-                                    <li><a href="#/"><i className="ti-twitter-alt"></i></a></li>
-                                    <li><a href="#/"><i className="ti-instagram"></i></a></li>
-                                </ul>
-                            </div>
+                        <div className="col-lg-3 text-center text-lg-end bride wow fadeInDown mt-5" data-wow-delay="0.1s" data-wow-duration="1.2s" >                            
+                            <p>¡Llegó el día!</p>
+                            <p>Queda en el recuerdo mi niñez y comienza una etapa que me permitirá reconocer nuevos retos y metas para cumplir mis sueños.</p>
+                            <p>Estoy construyendo mi propio camino y deseso compartirlo rodeada de todos los que quiero.</p>
+                            <h3>Mis papás</h3>                            
+                            <p>Jessica Herrera Tapia</p>
+                            <p>Javier Vargas Campos</p>
                         </div>
                         <div className="col-lg-6 text-center wow fadeInUp" data-wow-delay="0.1s" data-wow-duration="1.2s">
                             <div className="middle-couple-pic">
-                                <Image src={require('../assets/images/intro/1.jpg')} alt="image" className='img-fluid wow zoom' data-wow-delay='0.2s' data-wow-duration='1.5s' />
+                                <Image src={require('../assets/images/intro/1.jpeg')} alt="image" className='img-fluid wow zoom' data-wow-delay='0.2s' data-wow-duration='1.5s' />
                                 <div className="frame-img">
                                     <Image src={require('../assets/images/intro/shape.png')} alt="image" className='img-fluid' />
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-3 text-center text-lg-start groom wow fadeInDown" data-wow-delay="0.1s" data-wow-duration="1.2s">
-                            <div className="couple-img">
-                                <Image src={require('../assets/images/intro/3.jpg')} alt="image" className='img-fluid' width="100%" />
-                            </div>
-                            <h3>Manisha Tank</h3>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                            <div className="social">
-                                <ul>
-                                    <li><a href="#/"><i className="ti-facebook"></i></a></li>
-                                    <li><a href="#/"><i className="ti-twitter-alt"></i></a></li>
-                                    <li><a href="#/"><i className="ti-instagram"></i></a></li>
-                                </ul>
-                            </div>
+                        <div className="col-lg-3 text-center text-lg-start groom wow fadeInDown" data-wow-delay="0.1s" data-wow-duration="1.2s">                            
+                            <h3>Mis padrinos</h3>
+                            <p>Emilia Conde Flores</p>                        
+                            <p>Jorge Campos Fuentes</p> 
+                            {/* TODO: Agregar una cruz catolica  */}                                                        
                         </div>
                     </div>
                 </Container>
